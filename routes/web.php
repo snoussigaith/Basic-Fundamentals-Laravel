d7ee0badc1a3dbb62d7b5a878c969c3e580f29d2
snoussigaith <snoussi_gaith95@outlook.fr>
2024-01-27 21:46:40 +0100

    how to make controller and get url request

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +11,5 @@ Route::get('/', function () {
 Route::get('/about', function () {
 return view('about');
 });
-Route::get('/contact', function () {
-    return view("contact");
-    });
+Route::get('/contact','ContactController@index'); // laravel 7
+Route::get('/contact', [ContactController::class, 'index']); // laravel v8+
