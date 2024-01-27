1ce210a3ea66600659295cdadf9ab2e972f5609a
snoussigaith <snoussi_gaith95@outlook.fr>
2024-01-27 23:00:25 +0100

    middleware

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,11 @@ use App\Http\Controllers\ContactController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    echo "this is home page";
+});
 Route::get('/about', function () {
 return view('about');
-});
+})->middleware('age');
 Route::get('/contact','ContactController@index'); // laravel 7
 Route::get('/contact', [ContactController::class, 'index']); // laravel v8+
